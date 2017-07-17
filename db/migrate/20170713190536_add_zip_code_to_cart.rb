cbc80c3b6de12da45caf00ac6cdd1234d4870990
vipvex <aleksander.battle@gmail.com>
2017-07-17 00:23:36 +0000

    Fixed string misspelling error in migration

--- db/migrate/20170713190536_add_zip_code_to_cart.rb
+++ db/migrate/20170713190536_add_zip_code_to_cart.rb
@@ -1,6 +1,6 @@
 class AddZipCodeToCart < ActiveRecord::Migration
   def change
     add_column :carts, :zip_code, :string
-    add_column :carts, :country, :strig
+    add_column :carts, :country, :string
   end
 end
