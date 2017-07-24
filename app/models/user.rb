1e9c96c18ec6d3ff0b7b82ee4b579d1ae3cc5c5d
vipvex <aleksander.battle@gmail.com>
2017-07-24 05:17:08 +0000

    Fixed cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,6 @@ class User < ActiveRecord::Base
   private
   
   def create_cart
-    Card.create(user_id: self.id, shipping_option_id: ShippingOption.first.id)
+    Cart.create(user_id: self.id, shipping_option_id: ShippingOption.first.id)
   end
 end
